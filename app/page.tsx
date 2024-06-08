bcebdfa2660e84d5e9de01adecaef7445eab1da5
Md Noorullah Raza <mnraza94@gmail.com>
2024-06-08 00:28:46 +0000

    In Home page added description

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello There</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsa ab animi ipsam veniam voluptatum molestiae libero porro, nisi consectetur consequatur debitis, maiores magni. Doloribus vero impedit tempora reiciendis sapiente.
+      </p>
 
      
 
